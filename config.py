fb5fb6a4b58383cabcc149233f574f42fa71a2c3
PARK JIHUN <102800414+zzakjista@users.noreply.github.com>
2024-05-07 20:55:36 +0900

    서비스, 모델 config 추가

---
+++ config.py
@@ -0,0 +1,27 @@
+import torch
+class ModelConfig:
+    """
+    model 관련된 파라미터를 관리하는 config class
+    """
+    def __init__(self):
+        self.model_name:str = 'AutoRec'
+        self.version:str = 'v0.0'
+        self.batch_size:int = 32
+        self.epochs:int = 100
+        self.lr:float = 0.005
+        self.optimizer:str = 'adam'
+        self.criterion:str = 'mse'
+        self.hidden_units:int = 500
+        self.lamda_regularizer:float = 0.001
+        self.seed:int = 42  
+        self.verbose:int = 1
+        self.device:str = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+class ServiceConfig:
+    """
+    Service와 관련된 파라미터를 관리하는 config class
+    """
+    def __init__(self):
+        self.game_name:str = 'steam_games' # ['steam_games', 'amazon_games']
+
